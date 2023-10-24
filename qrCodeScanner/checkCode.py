3dcbebab85b5b274d8548a0e53f9be01321b0a4b
HelloImJohn <heyimactuallyjohn@gmail.com>
2023-10-24 08:10:01 +0200

    first version with activation of delete function that is depentant on validity of code on burron press.

--- qrCodeScanner/checkCode.py
+++ qrCodeScanner/checkCode.py
@@ -41,40 +41,25 @@ def startCam():
                     frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
 
             cv2.imshow(window_name, frame)
+        if cv2.waitKey(delay) & 0xFF == ord(' '):
+
+            print("this is bannanas")
+            if not code == None:
+                try:
+                    useCoupon(code)
+                except:
+                    print("NO CODE SCANNED")
+            else:
+                print("CODE NO LONGER VALID!")
 
         if cv2.waitKey(delay) & 0xFF == ord('q'):
             break
 
 #trying to find workaround multithreading and using this function
 def useCoupon(coupon):
-    t2 = Thread(target=startCam())
-    t2.start()
-    def on_press(key):
-        try:
-            print('alphanumeric key {0} pressed'.format(
-                key.char))
-        except AttributeError:
-            print('special key {0} pressed'.format(
-                key))
-
-    def on_release(key):
-        print('{0} released'.format(
-            key))
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False
-
-    # Collect events until released
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
-
-    # ...or, in a non-blocking fashion:
-    listener = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
-    listener.start()
+    df = pd.read_csv(r"idList.csv")
+    df = df.drop(coupon)
+    print("\033[1;32m Coupon turned in successfully")
 
 def checkQR(a):
     tempRem = a
@@ -86,11 +71,13 @@ def checkQR(a):
 
         #toBeDel = df[df['ID'].str.match(tempRem)].index[0]
         #df = df.drop(toBeDel)
-        print("VALID")
+        print("\033[1;32m VALID")
 
         return(df[df['ID'].str.match(tempRem)].index[0])
     else:
-        print("NOT VALID")
+        print("\033[93m NOT VALID")
+
+        return None
     #print(df)
 
     #df.to_csv('idList.csv', encoding='utf-8', index=False)
